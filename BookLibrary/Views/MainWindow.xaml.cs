3e575c9d305cab798ff34a0560b861bdb19ad40e
Ali Ahmadov <aahmadov7626@ada.edu.az>
2022-11-30 15:52:51 +0400

    first commit

---
+++ BookLibrary/Views/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BookLibrary.ViewModels;
+using BookLibrary.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookLibrary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var view = new HomeUC();
+            var viewModel = new HomeViewModel();
+            view.DataContext = viewModel;
+            this.DataContext = viewModel;
+
+            App.MyGrid = myGrid;
+            App.MyGrid.Children.Add(view);
+        }
+    }
+}
